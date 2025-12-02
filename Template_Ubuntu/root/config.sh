1e5550e40b73e7204747191935fd3c5ae2e29e25
xiaobo.guo <xiaobo.guo@joyworkshop.com>
2025-12-02 15:09:00 +0800

    支持 单独构建Image

--- Template_Ubuntu/root/config.sh
+++ Template_Ubuntu/root/config.sh
@@ -4,38 +4,16 @@ export http_proxy=http://host.docker.internal:${VPN_PORT}
 export https_proxy=http://host.docker.internal:${VPN_PORT}
 export all_proxy=http://host.docker.internal:${VPN_PORT}
 
-cp  /etc/apt/sources.list.d/ubuntu.sources   /etc/apt/sources.list.d/ubuntu.sources.bak
-
-# 替换为清华源
-echo "Types: deb
-URIs: http://mirrors.tuna.tsinghua.edu.cn/ubuntu
-Suites: noble noble-updates noble-backports
-Components: main restricted universe multiverse
-Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" > /etc/apt/sources.list.d/ubuntu.sources
-
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y iputils-ping vim tzdata git curl netcat-openbsd openssh-server dos2unix rsync
-
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-echo 'Asia/Shanghai' > /etc/timezone
-
-#git config --global http.proxy socks5://host.docker.internal:7890
-#git config --global https.proxy socks5://host.docker.internal:7890
-#git config --global user.email "test@test.com"
-#git config --global user.name "Ambition"
-#git config --global pull.rebase true
-
-#mkdir -p /root/.ssh
-#cp -r /data/host_ssh/* /root/.ssh/ 2>/dev/null || true
-rsync -av /data/host_ssh/ /root/.ssh
-# 复制 Template_Ubuntu 目录下的文件到对应位置
-rsync -av /data/Template_Ubuntu/root/ /root/
-# 1. 显式覆盖文件，不保留原目录结构
-#cp -fv /data/Template_Ubuntu/root/.ssh/config /root/.ssh/config
-#dos2unix /root/.ssh/ssh-proxy-wrapper.sh
+rsync -av /data/host_ssh/ /root/.ssh --exclude=ssh-proxy-wrapper.sh
+dos2unix /root/.ssh/ssh-proxy-wrapper.sh
 chmod 600 /root/.ssh/* 2>/dev/null || true
-chmod 700 /root/.ssh 2>/dev/null || true
+#chmod 700 /root/.ssh 2>/dev/null || true
 chmod +x ~/.ssh/ssh-proxy-wrapper.sh
+
+# 调试信息：显示环境变量值
+echo "DEBUG: HOST_IP = ${HOST_IP}"
+echo "DEBUG: VPN_PORT = ${VPN_PORT}"
+echo "DEBUG: PROXY_ADDR will be: ${HOST_IP}:${VPN_PORT}"
 
 cd /data/Git/DockerComposeGit
 git status
